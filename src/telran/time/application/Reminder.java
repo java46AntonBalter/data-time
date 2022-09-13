1c5d18f41dfb1c3a3d6f0ae81ca021067e2b292a
Anton Balter <antbalter@gmail.com>
2022-09-13 23:42:47 +0300

    hw26 solution

--- src/telran/time/application/Reminder.java
+++ src/telran/time/application/Reminder.java
@@ -21,40 +21,17 @@ public class Reminder {
 	private static int[] setReminderParams(String[] args) throws Exception {
 		if (args.length < 2) {
 			throw new Exception(
-					"please set the mandatory arguments - interval value and ChronoUnit enum string value (case insensitive)");
+					"please set the mandatory arguments - interval length and ChronoUnit enum string value (case insensitive)");
 		}
 		int[] res = { 10, 3600 };
 		ChronoUnit intervalUnit = getUnit(args[1]);
-		res[0] = getIntervalInSeconds(args[0], intervalUnit);
+		res[0] = getDurationInSeconds(args[0], intervalUnit);
 		if (args.length > 2) {
-			res[1] = getDurationInSeconds(intervalUnit, args[2]);
+			res[1] = getDurationInSeconds(args[2], intervalUnit);
 		}
 		return res;
 	}
-
-	private static int getDurationInSeconds(ChronoUnit unit, String duration) throws Exception {
-		int durationInUnits = getIntFromString(duration);
-		if (durationInUnits <= 0) {
-			throw new Exception("Duration value should be a positive number");
-		}
-		return Math.toIntExact(TimeUnit.of(unit).toSeconds(durationInUnits));
-	}
-
-	private static int getIntFromString(String duration) throws Exception {
-		int res;
-		try {
-			res = Integer.parseInt(duration);
-		} catch (NumberFormatException e) {
-			throw new Exception("value should be an integer number");
-		}
-		return res;
-	}
-
-	private static int getIntervalInSeconds(String amount, ChronoUnit unit) throws Exception {
-		int intervalValue = getIntervalValue(amount);
-		return Math.toIntExact(TimeUnit.of(unit).toSeconds(intervalValue));
-	}
-
+	
 	private static ChronoUnit getUnit(String unit) throws Exception{
 		ChronoUnit res = ChronoUnit.SECONDS;
 		try {
@@ -65,13 +42,21 @@ public class Reminder {
 		return res;
 	}
 
-	private static int getIntervalValue(String amount) throws Exception {
-		int res = getIntFromString(amount);
-		if (res <= 0) {
-			throw new Exception("Interval value should be a positive number");
+	private static int getDurationInSeconds(String duration, ChronoUnit unit) throws Exception {
+		int dur;
+		try {
+			dur = Integer.parseInt(duration);
+			if (dur <= 0) {
+				throw new Exception("Duration value should be a positive number");
+			}
+		} catch (NumberFormatException e) {
+			throw new Exception("value should be an integer number");
 		}
-		return res;
+		
+		return Math.toIntExact(TimeUnit.of(unit).toSeconds(dur));
 	}
+
+	
 
 	private static void playBeeps(int intervalInSeconds, int durationInSeconds) {
 		Instant current = Instant.now();
